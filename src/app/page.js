e5a06ea0fb73971efd783540298ae63bc59e2f9e
Xlr8th <saintgeorge.001@gmail.com>
2026-05-23 15:34:45 +0100

    force-dynamic

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Home from '../components/HomeClient'
 import { supabase } from '../lib/supabase'
 
+export const dynamic = 'force-dynamic'
+
 const page = async () => {
   const { data: posts, error } = await supabase
   .from('posts')
